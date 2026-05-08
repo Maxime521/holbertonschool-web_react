e81b0d38734b089d35bd10be7e872f6f34ae5feb
Maxime521 <9842@holbertonstudents.com>
2026-05-08 18:34:17 +0200

    I just modif the code

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -3,16 +3,17 @@ import _ from 'lodash';
 import '../css/main.css';
 
 let count = 0;
+
 function updateCounter() {
-    count++;
-    $('#count').text(`${count} clicks on the button`);
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
 }
 
-    $(function() {
-        $('body').append('<p>Holberton Dashboard</p>');
-        $('body').append('<p>Dashboard data for the students</p>');
-        $('body').append(`<button>Click here to get started</button>`);
-        $('body').append(`<p id="count"></p>`);
-        $('body').append(`<p>Copyright - Holberton School</p>`);
-        $('button').on('click', _.debounce(updateCounter, 500));
-    });
+$('body').append('<div id="logo"></div>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
+
+$('button').on('click', _.debounce(updateCounter, 500));
