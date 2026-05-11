86b939cdeb2d6d6e59f78f76b56abfad362d878b
Maxime521 <9842@holbertonstudents.com>
2026-05-12 00:41:26 +0200

    Modif gitignore

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'javascript/auto',
+        dependency: { not: ['url'] },
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              gifsicle: {
+                enabled: false,
+              },
+              mozjpeg: {
+                enabled: false,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                enabled: false,
+              },
+              svgo: {
+                enabled: false,
+              },
+              webp: {
+                enabled: false,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  performance: {
+    hints: false,
+  },
+};
